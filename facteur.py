80fdce9c6bd3ccd5e5c63989d706e29a7b68b4ec
outlouder <jopauc@gmail.com>
2015-01-10 18:22:01 +0100

    no message
    Signed-off-by: outlouder <jopauc@gmail.com>

--- facteur.py
+++ facteur.py
@@ -3,35 +3,7 @@ import csv
 from decimal import Decimal
 import os
 
-"""class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    CYAN = '\033[96m'
-    WHITE = '\033[97m'
-    YELLOW = '\033[93m'
-    MAGENTA = '\033[95m'
-    GREY = '\033[90m'
-    BLACK = '\033[90m'"""
-
-class bcolors:
-    HEADER = ''
-    OKBLUE = ''
-    OKGREEN = ''
-    WARNING = ''
-    FAIL = ''
-    ENDC = ''
-    CYAN = ''
-    WHITE = ''
-    YELLOW = ''
-    MAGENTA = ''
-    GREY = ''
-    BLACK = ''
-
-
+    
 class matos():
     
     def __init__(self,filename):
